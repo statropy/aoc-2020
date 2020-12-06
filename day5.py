525befb666c9be009a62db952624e349b8d1c7ba
Erik Larson <erik@statropy.com>
2020-12-06 12:36:19 -0500

    day 5 python: golf+efficient

--- day5.py
+++ day5.py
@@ -5,9 +5,8 @@ def next_seat():
         for line in f:
             yield(int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2))
 
-if __name__ == '__main__':
+def first_way():
     ids = [id for id in next_seat()]
-    print(max(ids))
     s = sorted(ids)
     offset = s[0]
     move = len(s) >> 1
@@ -19,4 +18,26 @@ if __name__ == '__main__':
             index += move
         else:
             index -= move
-    print(s[index]+1)
+    print(s[-1], s[index]+1)
+
+def golf():
+    seats = [int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2) for line in open('input5.txt', 'r')]
+    print(max(seats),[seat for seat in range(max(seats), min(seats), -1) if seat not in seats][0])
+
+def efficient():
+    seatmap = [0]*1024
+    max_id = 0
+    with open('input5.txt', 'r') as f:
+        for line in f:
+            id = int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2)
+            max_id = max(id, max_id)
+            seatmap[id] = 1
+        for i in range(max_id-1, 0, -1):
+            if seatmap[i] == 0:
+                print(max_id, i)
+                break
+
+if __name__ == '__main__':
+    first_way()
+    golf()
+    efficient()
