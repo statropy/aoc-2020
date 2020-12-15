86a65b91eca9495e839a0337089213208ee01d28
Erik Larson <erik@statropy.com>
2020-12-15 14:53:05 -0500

    day 15 C++ use find() for faster update

--- day15.cpp
+++ day15.cpp
@@ -9,9 +9,13 @@ int number_game(const std::list<int> & initial, int stop=2020)
     std::unordered_map<int, int> numbers;
     int last = 0;
     int turn = 1;
+
+    numbers.reserve(stop>>1);
+
     for (int n : initial) {
         numbers.insert({n,turn++});
     }
+
     for(; turn<stop; turn++) {
         int v = numbers[last];
         numbers[last] = turn;
@@ -19,6 +23,32 @@ int number_game(const std::list<int> & initial, int stop=2020)
     }
     return last;
 }
+
+int number_iterator(const std::list<int> & initial, int stop=2020)
+{
+    std::unordered_map<int, int> numbers;
+    int last = 0;
+    int turn = 1;
+
+    numbers.reserve(stop>>1);
+
+    for (int n : initial) {
+        numbers.insert({n,turn++});
+    }
+
+    for(; turn<stop; turn++) {
+        std::unordered_map<int, int>::iterator it = numbers.find(last);
+        if(it == numbers.end()) {
+            numbers.insert({last,turn});
+            last = 0;
+        } else {
+            last = turn - it->second;
+            it->second = turn;
+        }
+    }
+    return last;
+}
+
 
 int number_map(const std::list<int> & initial, int stop=2020)
 {
@@ -47,6 +77,8 @@ TEST(NumberGameTest, Part1) {
     ASSERT_EQ(438, number_game({3,2,1}));
     ASSERT_EQ(1836, number_game({3,1,2}));
     ASSERT_EQ(387, number_game({14,1,17,0,3,20}));
+    ASSERT_EQ(4, number_iterator({0,3,6},9));
+    ASSERT_EQ(436, number_iterator({0,3,6}));
 }
 
 TEST(NumberGameTest, Part2) {
@@ -58,6 +90,51 @@ TEST(NumberGameTest, Part2) {
     ASSERT_EQ(6895259, number_game({2,3,1},stop));
     ASSERT_EQ(18, number_game({3,2,1},stop));
     ASSERT_EQ(362, number_game({3,1,2},stop));
+    ASSERT_EQ(6428, number_game({14,1,17,0,3,20},stop));
+}
+
+TEST(NumberGameTest, Part2It) {
+    int stop = 30000000;
+    ASSERT_EQ(175594, number_iterator({0,3,6},stop));
+    ASSERT_EQ(2578, number_iterator({1,3,2},stop));
+    ASSERT_EQ(3544142, number_iterator({2,1,3},stop));
+    ASSERT_EQ(261214, number_iterator({1,2,3},stop));
+    ASSERT_EQ(6895259, number_iterator({2,3,1},stop));
+    ASSERT_EQ(18, number_iterator({3,2,1},stop));
+    ASSERT_EQ(362, number_iterator({3,1,2},stop));
+    ASSERT_EQ(6428, number_iterator({14,1,17,0,3,20},stop));
+}
+
+TEST(NumberGameTest, Part2a) {
+    int stop = 30000000;
+    ASSERT_EQ(175594, number_game({0,3,6},stop));
+}
+TEST(NumberGameTest, Part2b) {
+    int stop = 30000000;
+    ASSERT_EQ(2578, number_game({1,3,2},stop));
+}
+TEST(NumberGameTest, Part2c) {
+    int stop = 30000000;
+    ASSERT_EQ(3544142, number_game({2,1,3},stop));
+}
+TEST(NumberGameTest, Part2d) {
+    int stop = 30000000;
+    ASSERT_EQ(261214, number_game({1,2,3},stop));
+}
+TEST(NumberGameTest, Part2e) {
+    int stop = 30000000;
+    ASSERT_EQ(6895259, number_game({2,3,1},stop));
+}
+TEST(NumberGameTest, Part2f) {
+    int stop = 30000000;
+    ASSERT_EQ(18, number_game({3,2,1},stop));
+}
+TEST(NumberGameTest, Part2g) {
+    int stop = 30000000;
+    ASSERT_EQ(362, number_game({3,1,2},stop));
+}
+TEST(NumberGameTest, Part2h) {
+    int stop = 30000000;
     ASSERT_EQ(6428, number_game({14,1,17,0,3,20},stop));
 }
 
