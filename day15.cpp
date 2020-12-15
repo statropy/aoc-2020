2f8f2388a861ba12217e562258921d79630ad2a1
Erik Larson <erik@statropy.com>
2020-12-15 13:32:50 -0500

    day 15 std::map for time comparison

--- day15.cpp
+++ day15.cpp
@@ -1,8 +1,8 @@
 //day15.cpp
 #include <unordered_map>
+#include <map>
 #include <iostream>
 #include <list>
-#include <gtest/gtest.h>
 
 int number_game(const std::list<int> & initial, int stop=2020)
 {
@@ -20,7 +20,24 @@ int number_game(const std::list<int> & initial, int stop=2020)
     return last;
 }
 
+int number_map(const std::list<int> & initial, int stop=2020)
+{
+    std::map<int, int> numbers;
+    int last = 0;
+    int turn = 1;
+    for (int n : initial) {
+        numbers.insert({n,turn++});
+    }
+    for(; turn<stop; turn++) {
+        int v = numbers[last];
+        numbers[last] = turn;
+        last = (v == 0) ? 0 : turn - v;
+    }
+    return last;
+}
+
 #ifdef GTEST
+#include <gtest/gtest.h>
 TEST(NumberGameTest, Part1) {
     ASSERT_EQ(436, number_game({0,3,6}));
     ASSERT_EQ(1, number_game({1,3,2}));
@@ -42,6 +59,29 @@ TEST(NumberGameTest, Part2) {
     ASSERT_EQ(18, number_game({3,2,1},stop));
     ASSERT_EQ(362, number_game({3,1,2},stop));
     ASSERT_EQ(6428, number_game({14,1,17,0,3,20},stop));
+}
+
+TEST(NumberGameTest, Map1) {
+    ASSERT_EQ(436, number_map({0,3,6}));
+    ASSERT_EQ(1, number_map({1,3,2}));
+    ASSERT_EQ(10, number_map({2,1,3}));
+    ASSERT_EQ(27, number_map({1,2,3}));
+    ASSERT_EQ(78, number_map({2,3,1}));
+    ASSERT_EQ(438, number_map({3,2,1}));
+    ASSERT_EQ(1836, number_map({3,1,2}));
+    ASSERT_EQ(387, number_map({14,1,17,0,3,20}));
+}
+
+TEST(NumberGameTest, Map2) {
+    int stop = 30000000;
+    ASSERT_EQ(175594, number_map({0,3,6},stop));
+    ASSERT_EQ(2578, number_map({1,3,2},stop));
+    ASSERT_EQ(3544142, number_map({2,1,3},stop));
+    ASSERT_EQ(261214, number_map({1,2,3},stop));
+    ASSERT_EQ(6895259, number_map({2,3,1},stop));
+    ASSERT_EQ(18, number_map({3,2,1},stop));
+    ASSERT_EQ(362, number_map({3,1,2},stop));
+    ASSERT_EQ(6428, number_map({14,1,17,0,3,20},stop));
 }
 
 #else
