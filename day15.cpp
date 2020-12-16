ef32f8e811ebfdf1cc06f3e6e67034b9443edeba
Erik Larson <erik@statropy.com>
2020-12-16 10:40:32 -0500

    day 15 C++ and python perf experiments

--- day15.cpp
+++ day15.cpp
@@ -4,6 +4,31 @@
 #include <iostream>
 #include <list>
 
+int number_linear(const std::list<int> & initial, int stop=2020)
+{
+    int *numbers = (int*)malloc(sizeof(int)*(stop));
+    int last = 0;
+    int turn = 1;
+
+    if(!numbers) {return -1;}
+
+    memset(numbers, 0, sizeof(int)*stop);
+
+    for (int n : initial) {
+        numbers[n] = turn++;
+    }
+
+    for(; turn<stop; turn++) {
+        int v = numbers[last];
+        numbers[last] = turn;
+        last = (v == 0) ? 0 : turn - v;
+    }
+
+    free(numbers);
+
+    return last;
+}
+
 int number_game(const std::list<int> & initial, int stop=2020)
 {
     std::unordered_map<int, int> numbers;
@@ -105,68 +130,88 @@ TEST(NumberGameTest, Part2It) {
     ASSERT_EQ(6428, number_iterator({14,1,17,0,3,20},stop));
 }
 
-TEST(NumberGameTest, Part2a) {
+TEST(NumberGameTest, Part2Linear) {
     int stop = 30000000;
-    ASSERT_EQ(175594, number_game({0,3,6},stop));
-}
-TEST(NumberGameTest, Part2b) {
-    int stop = 30000000;
-    ASSERT_EQ(2578, number_game({1,3,2},stop));
-}
-TEST(NumberGameTest, Part2c) {
-    int stop = 30000000;
-    ASSERT_EQ(3544142, number_game({2,1,3},stop));
-}
-TEST(NumberGameTest, Part2d) {
-    int stop = 30000000;
-    ASSERT_EQ(261214, number_game({1,2,3},stop));
-}
-TEST(NumberGameTest, Part2e) {
-    int stop = 30000000;
-    ASSERT_EQ(6895259, number_game({2,3,1},stop));
-}
-TEST(NumberGameTest, Part2f) {
-    int stop = 30000000;
-    ASSERT_EQ(18, number_game({3,2,1},stop));
-}
-TEST(NumberGameTest, Part2g) {
-    int stop = 30000000;
-    ASSERT_EQ(362, number_game({3,1,2},stop));
-}
-TEST(NumberGameTest, Part2h) {
-    int stop = 30000000;
-    ASSERT_EQ(6428, number_game({14,1,17,0,3,20},stop));
+    ASSERT_EQ(175594, number_linear({0,3,6},stop));
+    ASSERT_EQ(2578, number_linear({1,3,2},stop));
+    ASSERT_EQ(3544142, number_linear({2,1,3},stop));
+    ASSERT_EQ(261214, number_linear({1,2,3},stop));
+    ASSERT_EQ(6895259, number_linear({2,3,1},stop));
+    ASSERT_EQ(18, number_linear({3,2,1},stop));
+    ASSERT_EQ(362, number_linear({3,1,2},stop));
+    ASSERT_EQ(6428, number_linear({14,1,17,0,3,20},stop));
 }
 
-TEST(NumberGameTest, Map1) {
-    ASSERT_EQ(436, number_map({0,3,6}));
-    ASSERT_EQ(1, number_map({1,3,2}));
-    ASSERT_EQ(10, number_map({2,1,3}));
-    ASSERT_EQ(27, number_map({1,2,3}));
-    ASSERT_EQ(78, number_map({2,3,1}));
-    ASSERT_EQ(438, number_map({3,2,1}));
-    ASSERT_EQ(1836, number_map({3,1,2}));
-    ASSERT_EQ(387, number_map({14,1,17,0,3,20}));
-}
+// TEST(NumberGameTest, Part2a) {
+//     int stop = 30000000;
+//     ASSERT_EQ(175594, number_game({0,3,6},stop));
+// }
+// TEST(NumberGameTest, Part2b) {
+//     int stop = 30000000;
+//     ASSERT_EQ(2578, number_game({1,3,2},stop));
+// }
+// TEST(NumberGameTest, Part2c) {
+//     int stop = 30000000;
+//     ASSERT_EQ(3544142, number_game({2,1,3},stop));
+// }
+// TEST(NumberGameTest, Part2d) {
+//     int stop = 30000000;
+//     ASSERT_EQ(261214, number_game({1,2,3},stop));
+// }
+// TEST(NumberGameTest, Part2e) {
+//     int stop = 30000000;
+//     ASSERT_EQ(6895259, number_game({2,3,1},stop));
+// }
+// TEST(NumberGameTest, Part2f) {
+//     int stop = 30000000;
+//     ASSERT_EQ(18, number_game({3,2,1},stop));
+// }
+// TEST(NumberGameTest, Part2g) {
+//     int stop = 30000000;
+//     ASSERT_EQ(362, number_game({3,1,2},stop));
+// }
+// TEST(NumberGameTest, Part2h) {
+//     int stop = 30000000;
+//     ASSERT_EQ(6428, number_game({14,1,17,0,3,20},stop));
+// }
 
-TEST(NumberGameTest, Map2) {
-    int stop = 30000000;
-    ASSERT_EQ(175594, number_map({0,3,6},stop));
-    ASSERT_EQ(2578, number_map({1,3,2},stop));
-    ASSERT_EQ(3544142, number_map({2,1,3},stop));
-    ASSERT_EQ(261214, number_map({1,2,3},stop));
-    ASSERT_EQ(6895259, number_map({2,3,1},stop));
-    ASSERT_EQ(18, number_map({3,2,1},stop));
-    ASSERT_EQ(362, number_map({3,1,2},stop));
-    ASSERT_EQ(6428, number_map({14,1,17,0,3,20},stop));
-}
+// TEST(NumberGameTest, Part1hL) {
+//     ASSERT_EQ(387, number_linear({14,1,17,0,3,20}));
+// }
+// TEST(NumberGameTest, Part2hL) {
+//     int stop = 30000000;
+//     ASSERT_EQ(6428, number_linear({14,1,17,0,3,20},stop));
+// }
+
+// TEST(NumberGameTest, Map1) {
+//     ASSERT_EQ(436, number_map({0,3,6}));
+//     ASSERT_EQ(1, number_map({1,3,2}));
+//     ASSERT_EQ(10, number_map({2,1,3}));
+//     ASSERT_EQ(27, number_map({1,2,3}));
+//     ASSERT_EQ(78, number_map({2,3,1}));
+//     ASSERT_EQ(438, number_map({3,2,1}));
+//     ASSERT_EQ(1836, number_map({3,1,2}));
+//     ASSERT_EQ(387, number_map({14,1,17,0,3,20}));
+// }
+
+// TEST(NumberGameTest, Map2) {
+//     int stop = 30000000;
+//     ASSERT_EQ(175594, number_map({0,3,6},stop));
+//     ASSERT_EQ(2578, number_map({1,3,2},stop));
+//     ASSERT_EQ(3544142, number_map({2,1,3},stop));
+//     ASSERT_EQ(261214, number_map({1,2,3},stop));
+//     ASSERT_EQ(6895259, number_map({2,3,1},stop));
+//     ASSERT_EQ(18, number_map({3,2,1},stop));
+//     ASSERT_EQ(362, number_map({3,1,2},stop));
+//     ASSERT_EQ(6428, number_map({14,1,17,0,3,20},stop));
+// }
 
 #else
 
 int main()
 {
-    std::cout << number_game({14,1,17,0,3,20}) << "\n"
-              << number_game({14,1,17,0,3,20}, 30000000) << "\n";
+    std::cout << number_linear({14,1,17,0,3,20}) << std::endl
+              << number_linear({14,1,17,0,3,20}, 30000000) << std::endl;
     return 0;
 }
 #endif
--- day15.py
+++ day15.py
@@ -1,13 +1,48 @@
 #day15.py
 import unittest
 
+def linear(initial, stop=2020):
+    last = 0
+    counts = [0]*stop
+    for i,v in enumerate(initial):
+        counts[v] = i+1
+    for turn in range(len(initial)+1, stop):
+        v = counts[last]
+        counts[last] = turn
+        if v == 0:
+            last = 0
+        else:
+            last = turn -v
+    return last
+
+
 def number_game(initial, stop=2020):
     last = 0
     numbers = {v:i+1 for i,v in enumerate(initial)}
     for turn in range(len(initial)+1, stop):
+        print(turn, last, len(numbers))
         v = numbers.setdefault(last, 0)
         numbers[last] = turn
         last = 0 if v == 0 else turn - v
+    return last
+
+def number_0(initial, stop=2020):
+    last = 0
+    zero = 0
+    numbers = {v:i+1 for i,v in enumerate(initial) if v > 0}
+    try:
+        zero = initial.index(0)+1
+    except:
+        pass
+
+    for turn in range(len(initial)+1, stop):
+        if last == 0:
+            last = turn - zero
+            zero = turn
+        else:
+            v = numbers.setdefault(last, 0)
+            numbers[last] = turn
+            last = 0 if v == 0 else turn - v
     return last
 
 class TestNumberGame(unittest.TestCase):
@@ -22,6 +57,26 @@ class TestNumberGame(unittest.TestCase):
         self.assertEqual(1836, number_game([3,1,2]))
         self.assertEqual(387, number_game([14,1,17,0,3,20]))
 
+    def run_part2(self, f):
+        stop = 30000000
+        self.assertEqual(175594, f([0,3,6],stop))
+        self.assertEqual(2578, f([1,3,2],stop))
+        self.assertEqual(3544142, f([2,1,3],stop))
+        self.assertEqual(261214, f([1,2,3],stop))
+        self.assertEqual(6895259, f([2,3,1],stop))
+        self.assertEqual(18, f([3,2,1],stop))
+        self.assertEqual(362, f([3,1,2],stop))
+        self.assertEqual(6428, f([14,1,17,0,3,20],stop))
+
+    def test_part2_game(self):
+        self.run_part2(number_game)
+
+    def test_part2_0(self):
+        self.run_part2(number_0)
+
+    def test_part2_linear(self):
+        self.run_part2(linear)
+
     def test_part2(self):
         stop = 30000000
         self.assertEqual(175594, number_game([0,3,6],stop))
@@ -34,4 +89,6 @@ class TestNumberGame(unittest.TestCase):
         self.assertEqual(6428, number_game([14,1,17,0,3,20],stop))
 
 if __name__ == '__main__':
-    unittest.main()
+    #unittest.main()
+    print(linear([0,3,6]))
+    print(linear([14,1,17,0,3,20],30000000))
